3db36872921ded2ceb93de5899cec353eded397e
Joshua Manchester <JManch@protonmail.com>
2024-07-05 14:24:59 +0100

    nix: remove xwayland overlay

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -25,7 +25,6 @@ in {
     inputs.hyprlang.overlays.default
     inputs.hyprwayland-scanner.overlays.default
     self.overlays.wayland-protocols
-    self.overlays.xwayland
 
     # Hyprland packages themselves
     (final: prev: let
@@ -63,16 +62,6 @@ in {
   hyprland-extras = lib.composeManyExtensions [
     inputs.xdph.overlays.xdg-desktop-portal-hyprland
   ];
-
-  # Patches XWayland's pkgconfig file to not include Cflags or includedir
-  # The above two variables trip up CMake and the build fails
-  xwayland = final: prev: {
-    xwayland = prev.xwayland.overrideAttrs (old: {
-      postInstall = ''
-        sed -i '/includedir/d' $out/lib/pkgconfig/xwayland.pc
-      '';
-    });
-  };
 
   wayland-protocols = final: prev: {
     wayland-protocols = prev.wayland-protocols.overrideAttrs (self: super: {
